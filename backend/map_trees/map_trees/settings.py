0c401e35e8d75dd8cc6d26e463151fe3f416bdd1
LeroyGorn <leroygorn@gmail.com>
2022-07-23 14:52:27 +0300

    Added main tree model

--- backend/map_trees/map_trees/settings.py
+++ backend/map_trees/map_trees/settings.py
@@ -74,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'map_trees.wsgi.application'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
--- backend/map_trees/tree/models.py
+++ backend/map_trees/tree/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Tree(models.Model):
+
+    class Status:
+        awful = 'awful'
+        bad = 'bad'
+        care_required = 'care_required'
+        good = 'good'
+        perfect = 'perfect'
+
+    STATUS_CHOICES = (
+        (Status.awful, 'Awful'),
+        (Status.bad, 'Bad'),
+        (Status.care_required, 'Care Required'),
+        (Status.good, 'Good'),
+        (Status.perfect, 'Perfect'),
+    )
+
+    radius = models.FloatField(
+        verbose_name='radius'
+    )
+    image = models.ImageField(
+        verbose_name='image',
+        default="default.png",
+        upload_to="covers/"
+    )
+    age = models.PositiveSmallIntegerField(
+        verbose_name='age'
+    )
+    species = models.CharField(
+        verbose_name='species',
+        max_length=128
+    )
+    condition = models.CharFieldField(
+        verbose_name='condition',
+        choices=STATUS_CHOICES,
+        default=Status.good
+    )
