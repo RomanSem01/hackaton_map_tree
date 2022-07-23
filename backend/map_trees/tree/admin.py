dd117008320b9936d78b5565317b9575c8565e65
Roman Semenenko <semenenkoroma01@gmail.com>
2022-07-23 16:59:31 +0300

    Added TreeWorkPlan to detail endpoint

--- backend/map_trees/tree/admin.py
+++ backend/map_trees/tree/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from tree.models import Tree
+from tree.models import Tree, TreeWorkPlan
 
 admin.site.register(Tree)
+admin.site.register(TreeWorkPlan)
--- backend/map_trees/tree/serializers.py
+++ backend/map_trees/tree/serializers.py
@@ -1,6 +1,7 @@
+from numpy import source
 from rest_framework import serializers
 
-from tree.models import Tree
+from tree.models import Tree, TreeWorkPlan
 
 
 class TreeListSerializer(serializers.ModelSerializer):
@@ -9,8 +10,15 @@ class TreeListSerializer(serializers.ModelSerializer):
         fields = ['id', 'latitude', 'longitude', 'radius']
 
 
+class TreeWorkPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreeWorkPlan
+        fields = ['id', 'plan']
+
 class TreeDetailSerializer(serializers.ModelSerializer):
+    tree_work_plans = TreeWorkPlanSerializer(allow_null=True, required=False, many=True)
+    
     class Meta:
         model = Tree
         fields = ['id', 'latitude', 'longitude', 'radius', 'age', 
-                 'family', 'condition', 'image']
+                 'family', 'condition', 'image', 'tree_work_plans']
